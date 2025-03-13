f198fa01495ef051e9a35a0f064a39558a908acd
Mateus Lemos <teuse853@gmail.com>
2025-03-13 14:53:56 -0300

    Cria o serviço UserService para gerenciar usuários.

--- ecommerce/src/main/java/br/mateus/entity/User.java
+++ ecommerce/src/main/java/br/mateus/entity/User.java
@@ -17,8 +17,6 @@ public class User {
     private String email;
     private String password;
 
-    public User() {
-    }
 
     public Long getId() {
         return id;
---
+++ ecommerce/src/main/java/br/mateus/services/UserService.java
@@ -0,0 +1,40 @@
+package br.mateus.services;
+
+import br.mateus.entity.User;
+import br.mateus.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser (User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+    }
+
+    public User updateUser (Long id, User userDetails) {
+        User user = getUserById(id);
+        user.setName(userDetails.getName());
+        user.setEmail(userDetails.getEmail());
+
+        return userRepository.save(user);
+    }
+
+    public void deleteUser (Long id) {
+        userRepository.deleteById(id);
+    }
+}
