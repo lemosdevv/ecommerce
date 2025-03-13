4edbd14278ffce8347183f11c2458a19813bdb0a
Mateus Lemos <teuse853@gmail.com>
2025-03-13 14:20:04 -0300

    first commit

---
+++ ecommerce/src/test/java/br/mateus/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mateus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
